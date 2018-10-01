89cf5e291c4efe510cd09184c6d30590dfe3a1a8
alberto13711 <alberto13711@gmail.com>
2018-09-30 20:15:46 -0500

    Más funciones y procedimientos

---
+++ SQL Server/fn_usuarios.sql
@@ -0,0 +1,12 @@
+create FUNCTION fn_mostrarUsuarios(@id int)
+RETURNS @TABLE TABLE
+(id int,
+nombre varchar(255),
+pass varchar(255))
+AS 
+BEGIN
+INSERT INTO @TABLE
+ select id, nombre, pass from dbo.usuarios WHERE (id = @id OR @id=0)
+RETURN 
+END
+GO
