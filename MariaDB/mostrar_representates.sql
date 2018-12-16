cf0b8dc4def244aaaa9acf50a87e7412cf48443f
alberto13711 <alberto13711@gmail.com>
2018-12-16 15:40:18 -0500

    Insertar datos de procedimientos en tablas temporales

--- MariaDB/mostrar_representates.sql
+++ MariaDB/mostrar_representates.sql
@@ -2,3 +2,5 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrar_representantes`(IN `nombres` VARCHAR(25))
 select cedula,nombre,apellido from representantes where nombre like concat('%',nombres,'%')$$
 DELIMITER ;
+
+call  mostrar_representantes("lu");
